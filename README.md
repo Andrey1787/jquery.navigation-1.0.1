8344e61cedbb5b329e927c61af9045cf57b01f5e
Andrey <andrey1987.gorbunov@yandex.ru>
2017-05-21 10:54:57 +0300

    edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,143 @@
 # jquery.navigation-1.0.1
+This plugin allows you to display a beautiful sliding menu with content on the custom sites effortlessly.Adaptive menu for screens larger than 991 pixels on small screens the menu is not displayed.
+# Demo: The Basics Topics
+## Setup
+1. Create a basic **navigation** markup.Let's start with the description of the div element with the class container-fluid, I chose it specifically to stretch the menu the full width of the screen, you can use any other.
+```html
+<body>
+    <div class="container-fluid">
+        <div class="wrap-navigation">
+            <div class="top-shadow"></div>
+            <div class="wrap">
+                <div class="nav-header">
+                    <ul class="navigation list-unstyled">
+                        <li>
+                            <a href="#part1" class="text-uppercase default">
+                                Part1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#part2" class="text-uppercase default">
+                                Part2
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#part3" class="text-uppercase default">
+                                Part3
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="nav-body">
+                    <div class="hide" id="part1">
+                        <h3 class="text-center">Part1</h3>
+                    </div>
+                    <div class="hide" id="part2">
+                        <h3 class="text-center">Part2</h3>
+                    </div>
+                    <div class="hide" id="part3">
+                        <h3 class="text-center">Part3</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+```
+2. To connect a file of styles.
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Your title</title>
+    <link rel="stylesheet" href="css/jquery.navigation-1.0.1.css">
+</head>
+</html>
+```
+3. This plugin can work with **bootstrap.css** but it has to be connected at the beginning.
+```html
+<head>
+    <meta charset="UTF-8">
+    <title>Your title</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/jquery.navigation-1.0.1.css">
+</head>
+```
+4. Now we need to connect before the closing body tag ***jquery*** file, I do it with ***google*** [go](https://developers.google.com/speed/libraries/#jquery).
+```html
+<body>
+    
+        ......
+    //your markup
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</body>
+```
+5. It remains to connect the plugin file.
+```html
+<body>
+    ......
+    ......
+    //your markup
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/jquery.navigation-1.0.1.js"></script>
+</body>
+```
+6. Now by calling one method we run the plugin.
+```html
+<body>
+    ......
+    ......
+    //your markup
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/jquery.navigation-1.0.1.js"></script>
+    <script>
+        $('ul.navigation').navigate();
+    </script>
+</body>
+```
+> That it will look like the menu in the beginning:
+
+![First Screen](http://savepic.ru/14011754.png)
+
+> After clicking on one of the menu items, content, seamlessly opens to the right, reaching the end goes down, but only on the first pressing.Beca just the content inside changes without reloading the page.See screenshot:
+
+![Second Screen](http://savepic.ru/14014826.png)
+
+# Documentation
+## Configurations
+
+> Example of a **navigation** object with configurations:
+
+    $('ul.navigate').navigate({
+        indent : 10,
+        height : 200,
+        speedX : 300,
+        speedY : 300,
+        width : 200
+    });
+
+## Basic Configuration
+
+***The configuration options are passed into the navigation as an object on creation.***
+
+| Config Property | Property Type | Example |      
+     Description         | Default |
+| :-------------: | :-----------: | :-----: | :---------: | :-----: |
+| indent          | integer       | 5       | Indent the content from the navigation menu | 10      |
+| height          | integer       | 150     | The height of the navigation menu            | 200     |
+| speedX          | integer       | 500     | The speed of opening the content width           | 300     |
+|speedY           | integer       | 500     | The speed of opening the content height          | 300     |
+| width           | integer       | 150     | The width of the navigation menu            | 200     |
+
+> Now, if you want to change the width and/or height of the navigation, you don't need to create a separate css file, and just pass these parameters to the **navigate** method in the configuration object.
+
+```js
+    $('ul.navigation').navigate({
+        width : 150,
+        height : 150
+    });
+```
+
+> You can also do with the rest of the parameters alternating them or assigning all together.
+> In the next version for convenience I will add some events.
---
+++ js/jquery.navigation-1.0.1.js
@@ -0,0 +1,228 @@
+;+function($){
+	$.event.special.open = {
+		bindType : 'click',
+		delegateType : 'click',
+		handle : function(e){
+			var data = $(this).data('ag.navigation');
+			if(data.opened)
+				return false;
+			return e.handleObj.handler.apply(this, arguments);
+		}
+	};
+
+	$.event.special.defaultLink = {
+		delegateType : 'click',
+		bindType : 'click',
+		handle : function(e){
+			return e.handleObj.handler.apply(this, arguments);
+		}
+	};
+
+	$.event.special.resizeMenu = {
+		delegateType : 'resize',
+		bindType : 'resize',
+		handle : function(e){
+			return e.handleObj.handler.apply(this, arguments);
+		}
+	};
+}(jQuery);
+
++function($){
+	var header = '.nav-header',
+		shadow = '.top-shadow',
+		wrap = '.wrap',
+		indentMenu = 8;
+
+	var Navigate = function(element, options){
+		this.$elem = $(element);
+		this.options = options;
+		this.container = '.wrap-navigation';
+		this.screen;
+		this.opened = false;
+		this.open = false;
+	};
+
+	Navigate.VERSION = '1.0.1';
+	Navigate.DEFAULTS = {
+		height : 200,
+		indent : 10,
+		speedX : 300,
+		speedY : 300,
+		width : 200
+	};
+
+	Navigate.prototype.init = function(){
+		this.fullWidth();
+		this.setContainerSize();
+		this.indentContent();
+		this.setHeaderSize();
+		this.setNavSize();
+		this.setShadowWidth();
+		this.setContentWidth();
+		this.setWrapperWidth();
+		this.transition();
+	};
+
+	Navigate.prototype.fullWidth = function(){
+		this.screen = document.body.clientWidth;
+	};
+
+	Navigate.prototype.indentContent = function(){
+		$('.nav-body').css('margin-left', this.options.indent);
+	};
+
+	Navigate.prototype.setHeaderSize = function(){
+		$(header).outerWidth(this.options.width);
+		$(header).outerHeight(this.options.height);
+	};
+
+	Navigate.prototype.setNavSize = function(){
+		this.$elem.outerWidth(this.options.width);
+		this.$elem.outerHeight(this.options.height);
+	};
+
+	Navigate.prototype.setShadowWidth = function(){
+		$(shadow).outerWidth(this.options.width);
+	};
+
+	Navigate.prototype.calculateWidth = function(){
+		return this.screen - this.options.width - this.options.indent;
+	};
+
+	Navigate.prototype.setContentWidth = function(){
+		var width = this.calculateWidth(),
+			content = this.$elem.parent().next();
+		content.outerWidth(width);
+	};
+
+	Navigate.prototype.setWrapperWidth = function(){
+		if($(this.container).is(':has("'+ wrap +'")')){
+			$(wrap).outerWidth(this.screen);
+		}
+	};
+
+	Navigate.prototype.setContainerSize = function(){
+		$(this.container).outerWidth(this.options.width);
+		$(this.container).outerHeight(this.options.height + indentMenu);
+	};
+
+	Navigate.prototype.openContent = function(){
+		this.opened = true;
+		this.openX();
+		$(this.container).outerWidth(this.screen);
+		this.active();
+		this.showContent();
+	};
+
+	Navigate.prototype.changeContent = function(){
+		this.active();
+		this.showContent();
+	};
+
+	Navigate.prototype.openX = function(){
+		$(this.container).css('transition', 'width ' + this.getSpeedX());
+	};
+
+	Navigate.prototype.openY = function(){
+		$(this.container).css('transition', 'height ' + this.getSpeedY());
+	};
+
+	Navigate.prototype.getSpeedX = function(){
+		var speed = this.options.speedX / 1000;
+		if(speed >= 1)
+			speed = parseInt(speed, 10) + 's';
+		else{
+			speed += "";
+			speed = speed.substr(-1, 1);
+			speed = '.' + speed + 's';
+		}
+
+		return speed;
+	};
+
+	Navigate.prototype.getSpeedY = function(){
+		var speed = this.options.speedY / 1000;
+		if(speed >= 1)
+			speed = parseInt(speed, 10) + 's';
+		else{
+			speed += "";
+			speed = speed.substr(-1, 1);
+			speed = '.' + speed + 's';
+		}
+
+		return speed;
+	};
+
+	Navigate.prototype.transition = function(){
+		var that = this;
+		$(this.container).on('transitionend oTransitionEnd webkitTransitionEnd', function(){
+			var curHeight = $(this).outerHeight(),
+				maxHeight = $('.nav-body').outerHeight() + indentMenu;
+			if(curHeight < maxHeight){
+				that.openY();
+				$(this).outerHeight(maxHeight);
+			}else{
+				that.opened = false;
+				that.open = true;
+			}
+		});
+	};
+
+	Navigate.prototype.active = function(){
+		var childs = this.$elem.children('li');
+		childs.removeClass('active');
+		(this.target.is('a') ? this.target.parent().addClass('active') : this.target.addClass('active'));
+	};
+
+	Navigate.prototype.showContent = function(){
+		var href = (this.target.is('a') ? this.target.attr('href') : this.target.children().attr('href')),
+			target = $(href);
+		target.parent().children().removeClass('show');
+		target.parent().children().addClass('hide');
+		target.removeClass('hide').addClass('show');
+	};
+
+	var Plugin = function(options){
+		return this.each(function(){
+			var $this = $(this),
+				data = $this.data('ag.navigation'),
+				option = $.extend({}, Navigate.DEFAULTS, $this.data(), (options || {}));
+			if(!data){
+				$this.data('ag.navigation', (data = new Navigate(this, option)));
+			}
+
+			if(!data.action){
+				data.init();
+				return;
+			}
+
+			var action = data.action;
+			data[action]();
+		});
+	};
+
+	$.fn.navigate = Plugin;
+	$.fn.navigate.Constructor = Navigate;
+
+	var clickHandler = function(e){
+		var data = $(this).data('ag.navigation');
+		data.target = $(e.target);
+		if(!data.open)
+			data.action = 'openContent';
+		else
+			data.action = 'changeContent';
+		Plugin.call($(this));
+	};
+
+	var handler = function(e){
+		e.preventDefault();
+	};
+
+	var resizeHandler = function(e){
+		console.log('RESIZE');
+	};
+
+	$(document).on('open', 'ul.navigation', clickHandler);
+	$(document).on('defaultLink', 'a.default', handler);
+	$(window).on('resizeMenu', resizeHandler);
+}(jQuery);
